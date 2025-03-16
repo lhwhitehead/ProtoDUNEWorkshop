538f1660f1e5e0af1c53f0e2da62307c79977bfb
Jacob Calcutt <calcuttj@msu.edu>
2025-03-16 15:32:59 -0500

    More example gallery scripts

--- dump_g4stage1.py
+++ dump_g4stage1.py
@@ -4,26 +4,31 @@ from gallery_utils import read_header, provide_list
 import numpy as np
 
 read_header('gallery/ValidHandle.h')
-mctruthv = 'std::vector<simb::MCTruth>'
-classes = [mctruthv]
+mcpartsv = 'std::vector<simb::MCParticle>'
+classes = [mcpartsv]
 provide_list(classes)
 
 if __name__ == '__main__':
   parser = ap()
   parser.add_argument('-f', type=str, required=True)
+  parser.add_argument('-n', type=int, default=-1)
+
   args = parser.parse_args()
   
   
   ev = RT.gallery.Event(RT.vector(RT.string)(1, args.f))
   nevents = ev.numberOfEventsInFile()
-  get_truths = ev.getValidHandle[mctruthv]
+  get_parts = ev.getValidHandle[mcpartsv]
   
   for i in range(nevents):
+    if args.n > 0 and i >= args.n: break
     ev.goToEntry(i)
-    truths = get_truths(RT.art.InputTag('generator'))
-    truth = truths.product()[0]
-    part = truth.GetParticle(0)
-    print('Event:', i, part.PdgCode(), f'{part.P():.2f}', part.NumberDaughters())
+    parts_prod = get_parts(RT.art.InputTag('largeant'))
+    part = parts_prod.product()[0]
+    print('Event:', i, f'PDG:{part.PdgCode()}', 
+          f'P:{part.P():.2f}', f'Daughters:{part.NumberDaughters()}')
     print(f'\tStart Position: {part.Position().X():.2f}, '
           f'{part.Position().Y():.2f}, {part.Position().Z():.2f}')
-    print('\tN particles:', truth.NParticles())
+    print(f'\tEnd Position: {part.EndPosition().X():.2f}, '
+          f'{part.EndPosition().Y():.2f}, {part.EndPosition().Z():.2f}')
+    print(f'\tEnd Process: {part.EndProcess()}')
